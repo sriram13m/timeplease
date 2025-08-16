779c89dc466a4e9d9cc7f96038d7ae76ceedf19e
sriram13m <sriram13k@gmail.com>
2025-08-17 01:25:09 +0530

    Support commonly used timezones
    Support commonly used timezones using chrono-tz and relevant changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,29 @@
 pub mod cli;
 
 use cli::{Args, Commands};
+use std::fs;
+use std::collections::HashMap;
+use chrono_tz::Tz;
+
+fn load_timezone_mapping() -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
+    let timezone_data = fs::read_to_string("src/timezones.json")?;
+
+    let timezone_mapping : HashMap<String, String> = serde_json::from_str(&timezone_data)?;
+
+    Ok(timezone_mapping)
+}
+
+fn parse_timezone(tz_name: &str) -> Result<Tz, Box<dyn std::error::Error>> {
+    let timezone_mappings = load_timezone_mapping()?;
+
+    let lowercase_name = tz_name.to_lowercase();
+
+    if let Some(iana_name) = timezone_mappings.get(&lowercase_name) {
+        return iana_name.parse::<Tz>().map_err(|e| e.into())
+    }
+
+    tz_name.parse::<Tz>().map_err(|e| e.into())
+}
 
 pub fn run(args: Args) -> Result<(), Box<dyn std::error::Error>> {
     match args.command {
